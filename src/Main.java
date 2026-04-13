af22664e7c9ac9815a051071456ac9102d988e1c
Robo200m <majumo05@hotmail.com>
2026-04-13 17:52:48 -0400

    completed lab 11

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        String menuChoice;
+        String[] list = new String[100];
+        int numOfItems  = 1;
+        do {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            System.out.println("Please choose an option from the following: ");
+            System.out.println("A - Add an item to list  ");
+            System.out.println("D - Delete item in list  ");
+            System.out.println("I - Insert item into list");
+            System.out.println("P - Print the list       ");
+            System.out.println("Q - Quit the program     ");
+            menuChoice = in.nextLine().toUpperCase();
+
+            if (menuChoice.equals("A")) {
+                System.out.println("Enter the name of the item you want to add: ");
+                String itemName = in.nextLine();
+                list[numOfItems] = itemName;
+                numOfItems++;
+
+            } else if (menuChoice.equals("D")) {
+                System.out.println("Enter the number of the item you want to delete: ");
+                int itemNum = Integer.parseInt(in.nextLine());
+                if (itemNum < 1 || itemNum > numOfItems) {
+                    System.out.println("Invalid input. Please try again.");
+                } else {
+                    int index = itemNum - 1;
+
+                    for (int i = index; i < numOfItems - 1; i++) {
+                        list[i] = list[i + 1];
+                        numOfItems--;
+                        System.out.println("Item deleted.");
+                    }
+                }
+
+
+            } else if (menuChoice.equals("I")) {
+                System.out.println("Enter the position of the item you want to insert: ");
+                int position = Integer.parseInt(in.nextLine());
+
+                if (position < 1 || position > numOfItems + 1) {
+                    System.out.println("Invalid position. Please try again.");
+                } else {
+                    System.out.println("Enter the name of the item you want to insert:");
+                    String itemName = in.nextLine();
+                    int index = position - 1;
+                    for (int i = numOfItems; i > index; i--) {
+                        list[i] = list[i - 1];
+                        list[index] = itemName;
+                        System.out.println("Item inserted.");
+                        numOfItems++;
+                    }
+                }
+
+            } else if (menuChoice.equals("P")) {
+                System.out.println("The list of items:  ");
+
+                if (numOfItems == 0) {
+                    System.out.println("There are no items in the list");
+                } else {
+                    for (int i = 0; i < numOfItems; i++) {
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
+                }
+            }
+            if (menuChoice.equals("Q")) {
+                System.out.println("Are you sure you would like to quit the program? (y/n)");
+                String answer = in.nextLine().toLowerCase();
+                if (answer.equals("y")) {
+                    System.out.println("Quitting the program.");
+                } else if (answer.equals("n")) {
+                    System.out.println("Returning to program.");
+                    menuChoice = "";
+                }
+            }
+        } while (!menuChoice.equals("Q"));
     }
 }
